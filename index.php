c87814afa08d0ae146856f83c058951701567b5b
AriestaPurwa <85605545+AriestaPurwa@users.noreply.github.com>
2021-12-07 13:08:33 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+        <form action="" method="">
+            <table>
+                <tr>
+                    <td>Nama</td>
+                    <td>:</td>
+                    <td><input type="text" name="nama"></td>
+                </tr>
+                <tr>
+                    <td>jenis kelamin</td>
+                    <td>:</td>
+                    <td><input type="text" name="jk"></td>
+                </tr>
+                <tr>
+                    <td>agama</td>
+                    <td>:</td>
+                    <td><input type="text" name="agama"></td>
+                </tr>
+                <tr>
+                    <td>alamat</td>
+                    <td>:</td>
+                    <td><textarea name="alamat"></textarea></td>
+                </tr>
+                <tr>
+                    <td>tempat lahir</td>
+                    <td>:</td>
+                    <td><input type="text" name="tl"></td>
+                </tr>
+                <tr>
+                    <td>Tanggal Lahir</td>
+                    <td>:</td>
+                    <td><input type="date" name="tla"></td>
+                </tr>
+                <tr>
+                    <td>hobi</td>
+                    <td>:</td>
+                    <td><input type="text" name="hobi"></td>
+                </tr>
+                <tr>
+                    <td>cita cita</td>
+                    <td>:</td>
+                    <td><input type="text" name="cc"></td>
+                </tr>
+                <tr>
+                    <td>usia</td>
+                    <td>:</td>
+                    <td><input type="number" name="usia"></td>
+                </tr>
+            </table>
+    </h1>    
+</body>
+</html>
